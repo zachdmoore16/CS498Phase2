b3329e0a593a3388dd7a0f222d2bb285f5ea2468
Zachary Moore <zachdmoore@gmail.com>
2019-11-01 17:32:39 -0400

    Adding correct files

---
+++ knexfile.js
@@ -0,0 +1,40 @@
+const common = {
+	client: 'postgresql',
+	migrations: {
+		directory: './src/main/migrations'
+	},
+	pool: {
+		min: 2,
+		max: 10
+	}
+}
+
+module.exports = {
+  development: Object.assign({
+    connection: {
+      database: 'abet_system_dev',
+      user: 'postgres',
+      password: 'password'
+    },
+    seeds: {
+      directory: './src/dev/seeds'
+    }
+	}, common),
+
+  test: Object.assign({
+    connection: {
+      database: 'abet_system_test',
+      user: 'postgres',
+      password: 'password'
+    },
+    seeds: {
+      directory: './src/test/seeds'
+    }
+  }, common),
+
+  production: Object.assign({
+    connection: {
+      database: 'abet_system'
+    }
+  }, common)
+};
